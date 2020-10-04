0b7c0455bd9d787fafb3c8a3b72a2ad20b9a4d38
hongge199 <1259131259@qq.com>
2020-10-04 16:48:39 +0800

    公共页面

---
+++ Laravel/resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('name','关于')
+@section('content')
+    <h1>about pages!!!</h1>
+@endsection
---
+++ Laravel/resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('name','帮助')
+@section('content')
+   <h1>help page</h1>
+@endsection
